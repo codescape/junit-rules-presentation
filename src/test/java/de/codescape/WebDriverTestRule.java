dffea96eb19f7cfbfe40354848b07e6e72389ea0
Stefan Glase <stefan.glase@googlemail.com>
2015-01-13 20:37:01 +0100

    add ClassRule for WebDriver

---
+++ src/test/java/de/codescape/WebDriverTestRule.java
@@ -0,0 +1,37 @@
+package de.codescape;
+
+import org.junit.rules.TestRule;
+import org.junit.runner.Description;
+import org.junit.runners.model.Statement;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+public class WebDriverTestRule implements TestRule {
+
+    private static ThreadLocal<WebDriver> driver = new ThreadLocal<WebDriver>();
+
+    public WebDriver getDriver() {
+        return driver.get();
+    }
+
+    @Override
+    public Statement apply(final Statement statement, Description description) {
+        return new Statement() {
+            @Override
+            public void evaluate() throws Throwable {
+                driver.set(new FirefoxDriver());
+                statement.evaluate();
+                closeDriverSafely();
+            }
+        };
+    }
+
+    private void closeDriverSafely() {
+        try {
+            driver.get().quit();
+        } catch (Exception e) {
+            // do not fail when closing browser fails
+        }
+    }
+
+}
